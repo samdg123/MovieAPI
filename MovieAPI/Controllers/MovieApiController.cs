84534f5fc14d5d9e17dea47bfd5bda377c8683fd
Sami <sami_a222@hotmail.com>
2021-11-13 18:31:58 +0000

    refactoring, polishing and added data as embedded resources

--- MovieAPI/Controllers/MovieApiController.cs
+++ MovieAPI/Controllers/MovieApiController.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text.Json;
 using System.Threading.Tasks;
 
@@ -13,94 +14,14 @@ namespace MovieAPI.Controllers
     [Route("[controller]")]
     public class MovieApiController : ControllerBase
     {
-        private static List<MetaData> MetaDataList;
-        private static List<MovieStatistic> MovieStatistics;
+        private static Repository Repo = new Repository();        
 
-        public MovieApiController()
-        {
-            if (MetaDataList == null)
-                MetaDataList = GetModelList_FromCsv<MetaData>(@"C:\metadata.csv");
-
-            if (MovieStatistics == null)
-                MovieStatistics = GetModelList_FromCsv<MovieStatistic>(@"C:\stats.csv");
-        }
-
-        private List<T> GetModelList_FromCsv<T>(string location) where T : Model
-        {
-            var modelList = new List<T>();
-            using (var reader = new StreamReader(location))
-            {
-                reader.ReadLine(); //discard first line
-                while (!reader.EndOfStream)
-                {
-                    var line = reader.ReadLine();
-
-                    var values1 = line.Split('"');
-                    var values = new List<string>();
-
-                    for (int i = 0; i < values1.Length; i++)
-                    {
-                        values1[i] = values1[i].Trim(',');
-
-                        if (i % 2 == 0)
-                            values.AddRange(values1[i].Split(','));
-                        else
-                            values.Add(values1[i]);
-                    }
-
-                    var model = GetModelFromStringArray<T>(values.ToArray());
-
-                    modelList.Add(model);
-                }
-            }
-            return modelList;
-        }
-
-        public T GetModelFromStringArray<T>(string[] values) where T : Model
-        {
-            Model model;
-
-            if (typeof(T) == typeof(MetaData))
-            {
-                var id = int.Parse(values[0]);
-                var movieId = int.Parse(values[1]);
-                var title = values[2];
-                var language = values[3];
-                var duration = TimeSpan.Parse(values[4]);
-                var releaseYear = int.Parse(values[5]);
-
-                model = new MetaData()
-                {
-                    Id = id,
-                    MovieId = movieId,
-                    Title = title,
-                    Language = language,
-                    Duration = duration,
-                    ReleaseYear = releaseYear
-                };
-            }
-            else if (typeof(T) == typeof(MovieStatistic))
-            {
-                var movieId = int.Parse(values[0]);
-                var watchDurationMs = int.Parse(values[1]);
-
-                model = new MovieStatistic()
-                {
-                    MovieId = movieId,
-                    WatchDurationMs = watchDurationMs
-                };
-            }
-            else
-                throw new Exception();
-
-            return (T)model;
-        }
-
+        #region Endpoints
         [HttpPost]
         [Route("metadata")]
         public IActionResult MetaData_Add([FromBody] MetaData metaData)
         {
-            MetaDataList.Add(metaData);
+            Repo.MetaDataList_Add(metaData);
             return Ok(metaData);
         }
 
@@ -108,14 +29,14 @@ namespace MovieAPI.Controllers
         [Route("metadata/{movieId:int}")]
         public IActionResult MetaData_GetByMovieId(int movieId)
         {
-            var results = MetaDataList.Where(m => m.MovieId == movieId)
-                                      .Where(m => m.GetType()
-                                                   .GetProperties()
-                                                   .All(p => !String.IsNullOrEmpty(p.GetValue(m)?.ToString() ?? "")))
-                                      .GroupBy(m => m.Language)
-                                      .Select(m => m.OrderByDescending(m1 => m1.Id).First())
-                                      .OrderBy(m => m.Language)
-                                      .ToList();
+            var results = Repo.MetaDataList.Where(m => m.MovieId == movieId)
+                                           .Where(m => m.GetType()
+                                                        .GetProperties()
+                                                        .All(p => !String.IsNullOrEmpty(p.GetValue(m)?.ToString() ?? "")))
+                                           .GroupBy(m => m.Language)
+                                           .Select(m => m.OrderByDescending(m1 => m1.Id).First())
+                                           .OrderBy(m => m.Language)
+                                           .ToList();
 
             if (results.Count == 0)
                 return NotFound();
@@ -127,23 +48,23 @@ namespace MovieAPI.Controllers
         [Route("movies/stats")]
         public IActionResult MovieStats_GetAll()
         {
-            var metaDataList = GetModelList_FromCsv<MetaData>(@"C:\metadata.csv");
+            var metaDataList = Repo.GetModelList_FromCsv<MetaData>("MovieAPI.Data.metadata.csv");
             var results = metaDataList.GroupBy(m => m.MovieId)
                                       .Select(m => m.First())
                                       .Select(m => new 
             { 
                 movieId = m.MovieId,
                 title = m.Title,
-                averageWatchDurationS = MovieStatistics.Where(ms => ms.MovieId == m.MovieId)
-                                                       .Average(ms => ms.WatchDurationMs) / 1000,
-                watches = MovieStatistics.Count(ms => ms.MovieId == m.MovieId),
+                averageWatchDurationS = Repo.MovieStatistics.Where(ms => ms.MovieId == m.MovieId)
+                                                            .Average(ms => ms.WatchDurationMs) / 1000,
+                watches = Repo.MovieStatistics.Count(ms => ms.MovieId == m.MovieId),
                 releaseYear = m.ReleaseYear
-            })
-                
+            })                
                 .OrderByDescending(m => m.watches)
                 .ThenByDescending(m => m.releaseYear);
 
             return Ok(results);
         }
+        #endregion
     }
 }
---
+++ MovieAPI/Repository.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace MovieAPI
+{
+    public class Repository
+    {
+        public List<MetaData> MetaDataList { get; set; }
+        public List<MovieStatistic> MovieStatistics { get; set; }
+
+        public Repository()
+        {
+            if (MetaDataList == null)
+                MetaDataList = GetModelList_FromCsv<MetaData>("MovieAPI.Data.metadata.csv");
+
+            if (MovieStatistics == null)
+                MovieStatistics = GetModelList_FromCsv<MovieStatistic>("MovieAPI.Data.stats.csv");
+        }
+
+        public void MetaDataList_Add(MetaData model)
+        {
+            MetaDataList.Add(model);
+        }
+
+        public List<T> GetModelList_FromCsv<T>(string fileName) where T : Model
+        {
+            var modelList = new List<T>();
+            Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(fileName);
+            using (var reader = new StreamReader(stream))
+            {
+                reader.ReadLine(); //discard first line as they are headers
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+
+                    var tempStrArray = line.Split('"');
+                    var values = new List<string>();
+
+                    for (int i = 0; i < tempStrArray.Length; i++)
+                    {
+                        tempStrArray[i] = tempStrArray[i].Trim(',');
+
+                        if (i % 2 == 0)
+                            values.AddRange(tempStrArray[i].Split(',')); //values with an even index are outside of speech marks and can be split by delimiter
+                        else
+                            values.Add(tempStrArray[i]); //don't split this by delimiter as it came from between speech marks
+                    }
+
+                    var model = GetModelFromStringArray<T>(values.ToArray());
+
+                    modelList.Add(model);
+                }
+            }
+            return modelList;
+        }
+
+        /// <summary>
+        /// Generic method for converting an array of values from a row in a csv file to a chosen model type.
+        /// If new models are to be used, code should be written to instruct how to parse the array
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="values"></param>
+        /// <returns></returns>
+        private T GetModelFromStringArray<T>(string[] values) where T : Model
+        {
+            Model model;
+
+            if (typeof(T) == typeof(MetaData))
+            {
+                var id = int.Parse(values[0]);
+                var movieId = int.Parse(values[1]);
+                var title = values[2];
+                var language = values[3];
+                var duration = TimeSpan.Parse(values[4]);
+                var releaseYear = int.Parse(values[5]);
+
+                model = new MetaData()
+                {
+                    Id = id,
+                    MovieId = movieId,
+                    Title = title,
+                    Language = language,
+                    Duration = duration,
+                    ReleaseYear = releaseYear
+                };
+            }
+            else if (typeof(T) == typeof(MovieStatistic))
+            {
+                var movieId = int.Parse(values[0]);
+                var watchDurationMs = int.Parse(values[1]);
+
+                model = new MovieStatistic()
+                {
+                    MovieId = movieId,
+                    WatchDurationMs = watchDurationMs
+                };
+            }
+            else
+                throw new NotImplementedException("Parsing this model is not implemented");
+
+            return (T)model;
+        }
+    }
+
+}
